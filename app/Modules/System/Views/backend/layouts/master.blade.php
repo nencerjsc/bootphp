bc740dea8a294bed74545f0ca26eedc88ca43e96
Neo Nguyen <hotronet@gmail.com>
2020-04-10 12:59:50 +0700

    up

--- app/Modules/System/Views/backend/layouts/master.blade.php
+++ app/Modules/System/Views/backend/layouts/master.blade.php
@@ -34,18 +34,6 @@
 <!-- AdminLTE App -->
 <script src="{{asset('adminlte/dist/js/adminlte.js')}}"></script>
 
-<!-- OPTIONAL SCRIPTS -->
-<script src="{{asset('adminlte/dist/js/demo.js')}}"></script>
-
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="{{asset('adminlte/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
-<script src="{{asset('adminlte/plugins/raphael/raphael.min.js')}}"></script>
-<script src="{{asset('adminlte/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
-<script src="{{asset('adminlte/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
-<!-- ChartJS -->
-<script src="{{asset('adminlte/plugins/chart.js/Chart.min.js')}}"></script>
-
 <!-- PAGE SCRIPTS -->
 <script src="{{asset('adminlte/dist/js/pages/dashboard2.js')}}"></script>
 </body>
